fef6ec78c670aa444be2cc547a56a7979f493b1a
Kai Qin <kai@codafi.com>
2022-08-10 14:12:36 +0800

    optimize code

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -7,11 +7,11 @@ from PIL import Image
 import PIL.Image
 import mimetypes
 
-s3_client = boto3.client('s3')
-
 def resize_image(image_path, resized_path):
   with Image.open(image_path) as image:
-      image.thumbnail(tuple(x / 2 for x in image.size))
+      # tuple(x / 2 for x in image.size)
+      size = 512, 512
+      image.thumbnail(size)
       image.save(resized_path)
 
 def resize_video(video_path, resized_path):
@@ -23,33 +23,41 @@ def lambda_handler(event, context):
       print("event: ", event)
       bucket = record['s3']['bucket']['name']
       key = unquote_plus(record['s3']['object']['key'])
-      tmpkey = key.replace('/', '')
-      download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
-      upload_path = '/tmp/resized-{}'.format(tmpkey)
-      s3_client.download_file(bucket, key, download_path)
-      type = mimetypes.MimeTypes().guess_type(download_path)
-      print(type)
+      handle_object(bucket, key)
 
-      try:
-        if type is None or type[0] is None:
-            upload_path = download_path
-        elif type[0].startswith("image/"):
-            resize_image(download_path, upload_path)
-        elif type[0].startswith("video/"):
-            resize_video(download_path, upload_path)
-            upload_path = download_path
-        else :
-            upload_path = download_path
-      except Exception as e:
-        print("Exception: ", e)
+
+def handle_object(bucket, key):
+  session = boto3.Session()
+  s3 = session.client('s3')
+  # s3 = session.resource('s3')
+
+  tmpkey = key.replace('/', '')
+  download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
+  upload_path = '/tmp/resized-{}'.format(tmpkey)
+  s3.download_file(bucket, key, download_path)
+  type = mimetypes.MimeTypes().guess_type(download_path)
+  # print(type)
+
+  try:
+    if type is None or type[0] is None:
         upload_path = download_path
-      
-      # upload
-      s3_client.upload_file(upload_path, '{}-resized'.format(bucket), key)
+    elif type[0].startswith("image/"):
+        resize_image(download_path, upload_path)
+    elif type[0].startswith("video/"):
+        resize_video(download_path, upload_path)
+        upload_path = download_path
+    else :
+        upload_path = download_path
+  except Exception as e:
+    print("Exception: ", e, key)
+    upload_path = download_path
+  
+  # upload
+  s3.upload_file(upload_path, '{}-resized'.format(bucket), key)
 
-      # clean tmp cache
-      if download_path == upload_path :
-        os.remove(download_path)
-      else :
-        os.remove(download_path)
-        os.remove(upload_path)
+  # clean tmp cache
+  if download_path == upload_path :
+    os.remove(download_path)
+  else :
+    os.remove(download_path)
+    os.remove(upload_path)
